28976ceb240796827b3a31c03988a5a147eaafde
renzolo34 <renzo.lopez@tecsup.edu.pe>
2023-07-10 16:13:19 -0500

    pago

--- models/Pagos.js
+++ models/Pagos.js
@@ -2,42 +2,43 @@ const { DataTypes } = require("sequelize");
 const { sequelize } = require("../config/db");
 const Comida = require("./Comida");
 
-const Pago = sequelize.define(
-  "Pago",
-  {
-    id: {
-      type: DataTypes.INTEGER.UNSIGNED,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    monto: {
-      type: DataTypes.DOUBLE,
-      allowNull: false,
-    },
-    fechaCompra: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
-      allowNull: false,
-    },
-    nombre_usuario: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    correo_usuario: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    comida: { 
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
+const Compra = sequelize.define('Compra', {
+  id: {
+    type: DataTypes.INTEGER.UNSIGNED,
+    primaryKey: true,
+    autoIncrement: true
   },
-  {
-    timestamps: false,
+  fechaCompra: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+    allowNull: false
+  },
+  nombreUsuario: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  correoUsuario: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  },
+  comidaArray: {
+    type: DataTypes.ARRAY(DataTypes.JSONB),
+    allowNull: false
+  },
+  total: {
+    type: DataTypes.DOUBLE,
+    allowNull: false
   }
-);
+}, {
+  timestamps: false
+});
 
-module.exports = Pago;
+Compra.belongsTo(Comida);
+
+module.exports = Compra;
 
 // const Comida = require('./Comida');
 // // Establece la relación muchos a muchos con Comida a través de PagoComida
