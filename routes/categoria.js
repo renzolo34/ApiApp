08f2a17427e36d69eb8b6004d4b71ead48416a2b
Josue Castillo <86072865+Josue9876@users.noreply.github.com>
2023-07-10 21:44:54 -0500

    Update categoria.js

--- routes/categoria.js
+++ routes/categoria.js
@@ -4,6 +4,7 @@ const categoriaController = require('../controllers/categoriaController');
 
 router.get('/',categoriaController.getCategoria);
 router.post('/',categoriaController.postCategoria);
-
+router.delete('/:id',categoriaController.deleteCategoria);
+router.put('/:id', categoriaController.deleteCategoria);
 
 module.exports = router;
